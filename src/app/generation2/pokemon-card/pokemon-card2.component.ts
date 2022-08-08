60f20d903fb3fbef1f45ec2b022aff78ba6b9837
gguikata <guikatayama.gt@gmail.com>
2022-08-08 11:07:21 -0300

    modificação pokemon card (status)

---
+++ src/app/generation2/pokemon-card/pokemon-card2.component.ts
@@ -0,0 +1,12 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-pokemon-card',
+//   templateUrl: './pokemon-card2.component.html',
+//   styleUrls: ['./pokemon-card2.component.scss'],
+// })
+// export class PokemonCardComponent implements OnInit {
+//   constructor() {}
+
+//   ngOnInit(): void {}
+// }
